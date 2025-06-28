fc21121284ade8b24179590204c86348f5f3b4b0
jyun27 <kimjyun27@gmail.com>
2025-06-29 05:52:21 +0900

    feat: 예시용 홈 컨트롤러 추가

--- src/main/java/com/example/medicare_call/MedicareCallApplication.java
+++ src/main/java/com/example/medicare_call/MedicareCallApplication.java
@@ -2,6 +2,8 @@ package com.example.medicare_call;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MedicareCallApplication {
@@ -10,4 +12,14 @@ public class MedicareCallApplication {
 		SpringApplication.run(MedicareCallApplication.class, args);
 	}
 
+}
+
+
+@RestController
+class HomeController {
+
+	@GetMapping("/")
+	public String home() {
+		return "Hello, Medicare-Call!";
+	}
 }
