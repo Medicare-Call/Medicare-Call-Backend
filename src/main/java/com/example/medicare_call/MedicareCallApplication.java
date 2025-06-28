f2cccfc53bab11ede9575ab5570dddfd915c0cb1
jyun27 <kimjyun27@gmail.com>
2025-06-29 03:53:41 +0900

    first commit

---
+++ src/main/java/com/example/medicare_call/MedicareCallApplication.java
@@ -0,0 +1,13 @@
+package com.example.medicare_call;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicareCallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicareCallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/medicare_call/MedicareCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.medicare_call;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicareCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
