9c551c3d8ea88510a7baec8dd9675af54a7b311f
jyun27 <kimjyun27@gmail.com>
2025-07-12 20:40:29 +0900

    feat: test코드에서 security 필터 비활성화

--- src/test/java/com/example/medicare_call/controller/ElderHealthInfoControllerTest.java
+++ src/test/java/com/example/medicare_call/controller/ElderHealthInfoControllerTest.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ElderHealthInfoController.class)
+@AutoConfigureMockMvc(addFilters = false)
 class ElderHealthInfoControllerTest {
     @Autowired MockMvc mockMvc;
     @MockBean ElderHealthInfoService elderHealthInfoService;
