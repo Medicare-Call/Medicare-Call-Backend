b29ea7eef12481342dcdd6efaf47b7fdef8059cf
jyun27 <kimjyun27@gmail.com>
2025-08-11 04:21:03 +0900

    docs: ElderSettingController 스웨거 설명 추가

--- src/main/java/com/example/medicare_call/controller/view/EldersSettingController.java
+++ src/main/java/com/example/medicare_call/controller/view/EldersSettingController.java
@@ -4,6 +4,7 @@ import com.example.medicare_call.dto.ElderSettingRequest;
 import com.example.medicare_call.dto.ElderSettingResponse;
 import com.example.medicare_call.global.annotation.AuthUser;
 import com.example.medicare_call.service.ElderSettingService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +22,10 @@ import java.util.List;
 public class EldersSettingController {
     private final ElderSettingService elderSettingService;
 
+    @Operation(
+            summary = "어르신 개인정보 설정 조회",
+            description = "로그인한 유저가 등록한 모든 어르신의 개인정보를 조회합니다."
+    )
     @GetMapping("/settings")
     public ResponseEntity<List<ElderSettingResponse>> getElderSettingInfo(@Parameter(hidden = true) @AuthUser Long memberId){
         log.info("어르신 설정 정보 조회 요청: memberId={}", memberId);
@@ -28,6 +33,10 @@ public class EldersSettingController {
         return ResponseEntity.ok(body);
     }
 
+    @Operation(
+            summary = "어르신 개인정보 설정 수정",
+            description = "어르신 정보 전체를 요청받아 값을 수정합니다."
+    )
     @PostMapping("/{elderId}/settings")
     public ResponseEntity<ElderSettingResponse> updateElderSettingInfo(
             @Parameter(hidden = true) @AuthUser Long memberId,
@@ -41,6 +50,10 @@ public class EldersSettingController {
         return ResponseEntity.ok(res);
     }
 
+    @Operation(
+            summary = "어르신 개인정보 설정 삭제",
+            description = "선택한 어르신의 개인정보를 삭제합니다."
+    )
     @DeleteMapping("/{elderId}/settings")
     public ResponseEntity<Void> deleteElderSettingInfo(
             @Parameter(hidden = true) @AuthUser Long memberId,
