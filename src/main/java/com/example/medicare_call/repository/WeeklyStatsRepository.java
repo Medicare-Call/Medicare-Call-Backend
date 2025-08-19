83b7f705e2ca15d6abc8743da30d15bc388c01e0
sudo-Terry <contact.terrykim@gmail.com>
2025-08-20 08:04:05 +0900

    edit: 미사용 테이블 제거 마이그레이션
    ### Desc
    - 주간 통계
      - 기존에는 CronJob으로 주기적으로 분석해놓고, 도메인 데이터를 조회하는 방식으로 구현하고자 했지만 실시간 반영으로 기획 변경되어 미사용
    - 회원 설정
      - 기존에는 별도 테이블로 관리하고자 했지만, Member 테이블에 통합하여 사용중
      - 데이터 분석에 사용될 것도 아니라면 굳이 과도하게 테이블을 나눌 필요는 없다

--- src/main/java/com/example/medicare_call/repository/WeeklyStatsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.medicare_call.repository;
-
-import com.example.medicare_call.domain.WeeklyStats;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface WeeklyStatsRepository extends JpaRepository<WeeklyStats, Integer> {
-} 
