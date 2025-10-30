91c258094039dae41e3d1bf90e1c2cc578fff5e8
jyun27 <kimjyun27@gmail.com>
2025-10-30 18:01:14 +0900

    fix: test 통과를 위한 MockBean 추가

--- src/test/java/com/example/medicare_call/MedicareCallApplicationTests.java
+++ src/test/java/com/example/medicare_call/MedicareCallApplicationTests.java
@@ -1,11 +1,20 @@
 package com.example.medicare_call;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 class MedicareCallApplicationTests {
+
+	@MockBean
+	private FirebaseApp firebaseApp;
+
+	@MockBean
+	private FirebaseMessaging firebaseMessaging;
 
 	@Test
 	void contextLoads() {
