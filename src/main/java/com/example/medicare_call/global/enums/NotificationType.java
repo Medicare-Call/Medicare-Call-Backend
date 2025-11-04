8c3712a3bfb67ace0daf89964ec1ce2c891f3ed3
jyun27 <kimjyun27@gmail.com>
2025-11-05 02:05:26 +0900

    refactor: NotificationType 이름 변경

--- src/main/java/com/example/medicare_call/global/enums/NotificationType.java
+++ src/main/java/com/example/medicare_call/global/enums/NotificationType.java
@@ -1,7 +1,7 @@
 package com.example.medicare_call.global.enums;
 
 public enum NotificationType {
-    CALL_COMPLETED,     // 케어콜 완료
+    CALL_STATUS_COMPLETED,     // 케어콜 완료
     HEALTH_ALERT,       // 건강 이상 징후
-    MISSED_CALL         // 부재중 알림
+    CALL_STATUS_NO_ANSWER         // 부재중 알림
 }
